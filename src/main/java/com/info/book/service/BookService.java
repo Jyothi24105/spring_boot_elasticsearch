a1c453f6a9a60a992be6ceaa1e88ff453d0e1a1d
jyothi <jyothi@jo-macbook-pro>
2018-02-20 23:17:20 -0500

    first

---
+++ src/main/java/com/info/book/service/BookService.java
@@ -0,0 +1,29 @@
+package com.info.book.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.info.model.Book;
+
+/**
+ * @author jyothi
+ *
+ */
+
+public interface BookService {
+
+	Book save(Book book);
+
+	void delete(Book book);
+
+	Book findOne(String id);
+
+	Iterable<Book> findAll();
+
+	Page<Book> findByAuthor(String author, PageRequest pageRequest);
+
+	List<Book> findByTitle(String title);
+
+}
